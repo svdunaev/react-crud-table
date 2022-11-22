5613036adbc70a275225a20dcba56ca2d2e44234
svdunaev <54572104+svdunaev@users.noreply.github.com>
2022-11-22 15:05:23 +0500

    Updates

---
+++ precache-manifest.82979b2050e4e57e81036dda5eb81677.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104113c8ff58914d25a5f757690028ad",
+    "url": "/react-crud-table/index.html"
+  },
+  {
+    "revision": "5fe19522fe80cff47e37",
+    "url": "/react-crud-table/static/css/main.9500efb5.chunk.css"
+  },
+  {
+    "revision": "7f9bf025ec923e0f01c1",
+    "url": "/react-crud-table/static/js/2.6105080c.chunk.js"
+  },
+  {
+    "revision": "5fe19522fe80cff47e37",
+    "url": "/react-crud-table/static/js/main.2a1b189b.chunk.js"
+  },
+  {
+    "revision": "0c70ec8026c314a7bf91",
+    "url": "/react-crud-table/static/js/runtime-main.e14a8de5.js"
+  }
+]);
